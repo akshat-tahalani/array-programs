18ca7a2b3c1107bc98ae6117d6d8e469416b9a26
akshat-tahalani <tahalani.akshat@gmail.com>
2026-02-26 04:49:26 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.97%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int n = s.size();
+        int end= n-1 ;
+        int count = 0 ; 
+
+
+        while(end >= 0 && s[end] == ' '){
+            
+            end--; 
+        }
+
+        while(end>=0 && s[end]!= ' '){
+            count++;
+            end--;
+        }
+        return count ;
+    }
+};
